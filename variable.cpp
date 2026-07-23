eab746845a577c1481ea0e6cb9e3133b74ca2efc
aakashownprep <aakashownprep@gmail.com>
2026-07-23 19:12:29 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,29 @@
+// //rules for variable
+// //space are not allowed
+// //starting of var alphabet and underscore _
+// //digits are not allowed when if
+// //starting declaration of var
+
+// int my name=100;
+// int my_name=100;
+// int 1a=34;
+// int a1=34;
+
+// age=valid
+// _total = valid 
+// student1 = valid 
+// total-marks= invalid 
+// float = invalid 
+// int = invalid 
+// emp_id = valid 
+// first name = invalid 
+
+#include<stdio.h>
+int main(){
+    //int float char long long long 
+    int age=10;
+    int AGE=20;
+    printf("%d",age);
+    printf("%d",AGE);
+    //case sensitive
+}
