e4d25cf23d1ca5b83b4ef2bbb510cebf54701457
aakashownprep <aakashownprep@gmail.com>
2026-07-23 19:12:00 +0530

    Add files via upload

---
+++ tocheckthesize.cpp
@@ -0,0 +1,10 @@
+//to check the size of data types
+#include<stdio.h>
+int main(){
+    printf("char = %zu bytes \n",sizeof(char));
+    printf("int = %zu bytes \n",sizeof(int));
+    printf("float = %zu bytes \n",sizeof(float));
+    printf("double = %zu bytes \n",sizeof(double));
+    printf("short int = %zu bytes \n",sizeof(short int));
+    printf("unsigned int  = %zu bytes \n",sizeof(unsigned int));
+}
